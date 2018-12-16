0c3d86c4bd1221141a763b40e2b06ce17f2a2ca9
Varisara N <varisara.n@gmail.com>
2018-12-16 05:09:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,8 +2,8 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    "Fizzbuzz"
+    "FizzBuzz"
   elsif int % 3 == 0
-  ""
+  "Fizz"
   end
 end
